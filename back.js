95a3a4635dbc9fa218b30896fe2f753151a45141
Centric <theminecrafter13772@gmail.com>
2024-10-15 06:55:12 -0700

    Update back.js

--- back.js
+++ back.js
@@ -1,50 +1,45 @@
 const {
     WORLD_PATH,
     BACKUP_PATH,
+    BACKUP_INTERVAL_MINUTES,
 } = require("./config.json");
-
 const fs = require("fs");
 
 function backupWorld() {
-    const timeNow = new Date();
-    const dateString =
-        timeNow.getMonth() +
-        "-" +
-        timeNow.getDay() +
-        "-" +
-        timeNow.getFullYear() +
-        " --  " +
-        timeNow.getHours() +
-        " " +
-        timeNow.getMinutes();
+    const date = new Date();
+    const timestamp =
+        date.getMonth() +
+        "_" +
+        date.getDay() +
+        "_" +
+        date.getFullYear().toString().slice(-2) +
+        "@" +
+        date.getHours() +
+        "_0" +
+        date.getMinutes();
 
-    const backedUpDir = BACKUP_PATH + "/" + dateString;
+    const createdBackup = BACKUP_PATH + "/" + timestamp;
 
-    fs.mkdir(backedUpDir, (err) => {
-        if (err) {
-            console.error(
-                "Error creating folder in backup directory || Error: " + err
+    fs.mkdir(createdBackup, (err) => {
+        if (err)
+            return console.error(
+                `[BACKUP.FAIL] FAILED TO CREATE BACKUP FOLDER!\n${err}`
             );
-        } else {
-            console.log("Created folder in backup directory");
-        }
     });
 
-    fs.cp(WORLD_PATH, backedUpDir, { recursive: true }, (err) => {
+    fs.cp(WORLD_PATH, createdBackup, { recursive: true }, (err) => {
         if (err) {
-            console.error(
-                "fatal error tryna copy. Please contact centrifag for support :) <3\nerror:\n" +
-                    err
-            );
-        } else {
-            console.log(
-                "Your faggot ass world has successfuly been schmoppied (ask centric for sloppy toppy) \n@" +
-                    BACKUP_PATH +
-                    "/" +
-                    dateString
+            return console.error(
+                `[BACKUP.FAIL] FAILED TO COPY FILES TO BACKUP FOLDER\n${err}`
             );
         }
+
+        console.log(`[BACKUP.SUCCESS] Backed Up: file:///${createdBackup}`);
     });
 }
 
-module.exports = backupWorld;
+function startBackup() {
+    setInterval(backupWorld, BACKUP_INTERVAL_MINUTES * 60000);
+}
+
+module.exports = { startBackup };
